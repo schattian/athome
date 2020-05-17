a85224e2be450f514a388891b292faacb831f03d
Sebastián Chamena <sebachamena@gmail.com>
2020-05-17 18:52:20 -0300

    backend/users: Remove all useless comments about migrations on connDB

--- backend/users/server/db.go
+++ backend/users/server/db.go
@@ -10,43 +10,10 @@ import (
 )
 
 func connDB() (*sqlx.DB, error) {
-	// wd, err := os.Getwd()
-	// if err != nil {
-	// return nil, errors.Wrap(err, "os.Getwd")
-	// }
-
-	// m, err := migrate.New(
-	// 	"file://"+wd+"/migrations",
-	// 	userconf.GetDATABASE_SRC(),
-	// )
-	// if err != nil {
-	// 	log.Fatal(errors.Wrap(err, "migrate.New"))
-	// }
-	// if err := m.Up(); err != nil {
-	// log.Fatal(errors.Wrap(err, "migrate.Up"))
-	// }
-
 	db, err := sqlx.Open("postgres", userconf.GetDATABASE_SRC())
-
 	if err != nil {
 		return nil, errors.Wrap(err, "sqlx.Open")
 	}
-	// driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
-	// if err != nil {
-	// 	return nil, errors.Wrap(err, "postgres.WithInstance")
-	// }
-	// migrations, err := migrate.NewWithDatabaseInstance(
-	// 	"file://"+wd+"/migrations",
-	// 	userconf.DATABASE_SCHEME, driver,
-	// )
-	// if err != nil {
-	// 	return nil, errors.Wrap(err, "migrate.NewWithDatabaseInstance")
-	// }
-	// err = migrations.Steps(1)
-	// if err != nil {
-	// 	return nil, errors.Wrap(err, "migrations.Steps")
-	// }
-
 	db.MapperFunc(name.ToSnakeCase)
 	return db, nil
 }
