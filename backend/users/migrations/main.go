8e076c46d95756c5ac3c8b6afa81eae4a428e8b8
Sebastián Chamena <sebachamena@gmail.com>
2020-05-17 19:16:46 -0300

    backend/users: Add full support of go bin instead of relying on go-migrate cli

--- backend/users/migrations/main.go
+++ backend/users/migrations/main.go
@@ -3,26 +3,53 @@ package main
 import (
 	"log"
 	"os"
-	"os/exec"
+	"strconv"
 
 	"github.com/athomecomar/athome/backend/users/userconf"
+
+	"github.com/golang-migrate/migrate/v4"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+
+	"github.com/pkg/errors"
 )
 
 func main() {
 	if len(os.Args) <= 1 {
-		log.Fatal("action not given (up|down|drop|force|goto)")
+		log.Fatal("action not given (up|down|drop|force)")
 	}
 	action := os.Args[1]
 
-	var opt string
+	var err error
+	var version int
 	if len(os.Args) > 2 {
-		opt = os.Args[2]
+		version, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatalf("%v version is not a number", os.Args[2])
+		}
 	}
-	cmd := exec.Command("bin/migrate", userconf.GetDATABASE_SRC(), action, opt)
-	stdout, err := cmd.Output()
-	log.Println(action)
+
+	m, err := migrate.New(
+		"file://migrations",
+		userconf.GetDATABASE_SRC(),
+	)
 	if err != nil {
-		log.Fatalf("cmd.Run: %v", err)
+		log.Fatal(errors.Wrap(err, "migrate.New"))
 	}
-	log.Println(stdout)
+	switch action {
+	case "up":
+		err = m.Up()
+	case "down":
+		err = m.Down()
+	case "drop":
+		err = m.Drop()
+	case "force":
+		err = m.Force(version)
+	}
+
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "migrate."+action))
+	}
 }
