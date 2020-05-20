a1dcb892341f4f76e67a2488f828bd379769beba
Sebastián Chamena <sebachamena@gmail.com>
2020-05-20 17:57:45 -0300

    backend/users: Add signUpShared

---
+++ backend/users/server/sign_up_shared.go
@@ -0,0 +1,61 @@
+package server
+
+import (
+	"context"
+
+	"github.com/athomecomar/athome/backend/users/ent"
+	"github.com/athomecomar/athome/backend/users/ent/field"
+	"github.com/athomecomar/athome/backend/users/pbuser"
+	"github.com/athomecomar/storeql"
+	"github.com/athomecomar/xerrors"
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) SignUpShared(ctx context.Context, in *pbuser.SignUpSharedRequest) (*pbuser.SignUpSharedResponse, error) {
+	if err := in.Validate(); err != nil {
+		return nil, err
+	}
+	db, err := connDB()
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "connDB: %v", err)
+	}
+	defer db.Close()
+
+	previous, err := fetchOnboardingByToken(ctx, db, in)
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "fetchOnboardingByToken: %v", err)
+	}
+
+	onboarding := signUpSharedRequestToOnboarding(previous, in).Next()
+
+	code, err := onboarding.MustStage(field.Shared)
+	if err != nil {
+		return nil, status.Errorf(code, "MustStage: %v", err)
+	}
+
+	code, err = onboarding.ValidateByStage(ctx, db)
+	if err != nil {
+		return nil, status.Errorf(code, "ValidateByStage: %v", err)
+	}
+
+	err = storeql.UpdateIntoDB(ctx, db, onboarding)
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "UpdateIntoDB: %v", err)
+	}
+
+	return onboardingToSignUpSharedResponse(onboarding), nil
+}
+
+func signUpSharedRequestToOnboarding(prev *ent.Onboarding, in *pbuser.SignUpSharedRequest) *ent.Onboarding {
+	prev.Surname = field.Surname(in.GetSurname())
+	prev.Name = field.Name(in.GetName())
+	prev.Email = field.Email(in.GetEmail())
+	return prev
+}
+
+func onboardingToSignUpSharedResponse(o *ent.Onboarding) *pbuser.SignUpSharedResponse {
+	return &pbuser.SignUpSharedResponse{
+		Token: o.Id,
+	}
+}
