15353699e930efee41b3895e6b26e42a43908ba2
Sebastián <sebachamena@gmail.com>
2020-05-23 19:57:34 -0300

    backend/users: Initial commit

---
+++ backend/auth/server/jwt.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"fmt"
+
+	"github.com/athomecomar/xerrors"
+	"github.com/dgrijalva/jwt-go"
+	"google.golang.org/grpc/status"
+)
+
+func claimJwt(token string, secretFn func() string) (jwt.MapClaims, error) {
+	claimableToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing alg: %v", token.Header["alg"])
+		}
+
+		return []byte(secretFn()), nil
+	})
+	if err != nil {
+		return nil, status.Error(xerrors.Internal, "jwt isnt parsable")
+	}
+
+	claims, ok := claimableToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, status.Error(xerrors.FailedPrecondition, "jwt isnt claimable")
+	}
+
+	if !claimableToken.Valid {
+		return nil, status.Errorf(xerrors.Unauthenticated, "claimable Jwt.Valid: %v", err)
+	}
+
+	if err := claims.Valid(); err != nil {
+		return nil, status.Errorf(xerrors.Unauthenticated, "claimed jwt.Valid: %v", err)
+	}
+
+	return claims, nil
+}
