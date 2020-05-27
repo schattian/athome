64d55ba39d620f4b5924e63d87a205dcfbfb9021
Sebastián <sebachamena@gmail.com>
2020-05-27 18:45:34 -0300

    backend/users: Add reset pwd rpc

---
+++ backend/users/server/reset_password.go
@@ -0,0 +1,55 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/athomecomar/athome/backend/users/ent"
+	"github.com/athomecomar/athome/backend/users/pbuser"
+	"github.com/athomecomar/athome/backend/users/userconf"
+	"github.com/athomecomar/storeql"
+	"github.com/athomecomar/xerrors"
+	"github.com/jmoiron/sqlx"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (s *Server) ResetPassword(ctx context.Context, in *pbuser.ResetPasswordRequest) (*emptypb.Empty, error) {
+	if err := in.Validate(); err != nil {
+		return nil, err
+	}
+	db, err := connDB()
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "connDB: %v", err)
+	}
+	defer db.Close()
+
+	return s.resetPassword(ctx, db, in)
+}
+
+func (s *Server) resetPassword(ctx context.Context, db *sqlx.DB, in *pbuser.ResetPasswordRequest) (*emptypb.Empty, error) {
+	userId, err := handleJwt(in.GetToken(), userconf.GetFORGOT_JWT_SECRET)
+	if err != nil {
+		return nil, err
+	}
+
+	u := &ent.User{}
+	row := db.QueryRowxContext(ctx, `SELECT * FROM users WHERE id=$1`, userId)
+	err = row.StructScan(u)
+	if err == sql.ErrNoRows {
+		return nil, status.Errorf(xerrors.NotFound, "StructScan: %v", err)
+	}
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "StructScan: %v", err)
+	}
+	err = u.AssignPassword(in.GetPassword())
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "AssignPassword: %v", err)
+	}
+	err = storeql.UpdateIntoDB(ctx, db, u)
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "storeql.UpdateIntoDB: %v", err)
+	}
+
+	return &emptypb.Empty{}, nil
+}
---
+++ backend/users/server/reset_password_test.go
@@ -0,0 +1,77 @@
+package server
+
+import (
+	"context"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/athomecomar/athome/backend/users/pbuser"
+	"github.com/athomecomar/storeql"
+	"github.com/athomecomar/storeql/test/sqlassist"
+	"github.com/athomecomar/storeql/test/sqlhelp"
+	"github.com/athomecomar/xerrors"
+	"google.golang.org/grpc/status"
+)
+
+func TestServer_resetPassword(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		in  *pbuser.ResetPasswordRequest
+	}
+	tests := []struct {
+		name       string
+		args       args
+		queryStubs []*sqlassist.QueryStubber
+		execStubs  []*sqlassist.ExecStubber
+		wantStatus xerrors.Code
+	}{
+		{
+			name: "basic change of pwd",
+			args: args{
+				ctx: context.Background(),
+				in:  &pbuser.ResetPasswordRequest{Token: gTokens.Forgot.Valid, Password: fooPwd},
+			},
+			queryStubs: []*sqlassist.QueryStubber{
+				{
+					Expect: "SELECT * FROM users",
+					Rows:   sqlmock.NewRows(storeql.SQLColumns(gUsers.Consumers.Foo)).AddRow(storeql.SQLValues(gUsers.Consumers.Foo)...),
+				},
+			},
+			execStubs: []*sqlassist.ExecStubber{
+				{
+					Expect: "UPDATE users SET", Result: sqlmock.NewResult(1, 1),
+				},
+			},
+			wantStatus: xerrors.OK,
+		},
+		{
+			name: "expired token",
+			args: args{
+				ctx: context.Background(),
+				in:  &pbuser.ResetPasswordRequest{Token: gTokens.Forgot.Expired, Password: fooPwd},
+			},
+			wantStatus: xerrors.InvalidArgument,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			db, mock := sqlhelp.MockDB(t)
+			for _, stub := range tt.queryStubs {
+				stub.Stub(mock)
+			}
+			for _, stub := range tt.execStubs {
+				stub.Stub(mock)
+			}
+			s := &Server{}
+			_, err := s.resetPassword(tt.args.ctx, db, tt.args.in)
+			if status.Code(err) != tt.wantStatus {
+				t.Fatalf("Server.resetPassword() error = %v, status: %v;  wantStatus %v", err, status.Code(err), tt.wantStatus)
+			}
+			if status.Code(err) != xerrors.OK {
+				return
+			}
+		})
+	}
+}
