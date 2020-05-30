c9c1a832dc1d183273c575a16a35da82e608102e
Sebastián <sebachamena@gmail.com>
2020-05-30 11:07:52 -0300

    pb: Add switchRole and signOut

--- backend/users/pb/pbuser/user.pb.validate.go
+++ backend/users/pb/pbuser/user.pb.validate.go
@@ -36,6 +36,157 @@ var (
 // define the regex for a UUID once up-front
 var _user_uuidPattern = regexp.MustCompile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
 
+// Validate checks the field values on SwitchRoleRequest with the rules defined
+// in the proto definition for this message. If any rules are violated, an
+// error is returned.
+func (m *SwitchRoleRequest) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for AccessToken
+
+	if _, ok := _SwitchRoleRequest_Role_InLookup[m.GetRole()]; !ok {
+		return SwitchRoleRequestValidationError{
+			field:  "Role",
+			reason: "value must be in list [service-provider consumer merchant]",
+		}
+	}
+
+	// no validation rules for Password
+
+	return nil
+}
+
+// SwitchRoleRequestValidationError is the validation error returned by
+// SwitchRoleRequest.Validate if the designated constraints aren't met.
+type SwitchRoleRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SwitchRoleRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SwitchRoleRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SwitchRoleRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SwitchRoleRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SwitchRoleRequestValidationError) ErrorName() string {
+	return "SwitchRoleRequestValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e SwitchRoleRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSwitchRoleRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SwitchRoleRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SwitchRoleRequestValidationError{}
+
+var _SwitchRoleRequest_Role_InLookup = map[string]struct{}{
+	"service-provider": {},
+	"consumer":         {},
+	"merchant":         {},
+}
+
+// Validate checks the field values on SignOutRequest with the rules defined in
+// the proto definition for this message. If any rules are violated, an error
+// is returned.
+func (m *SignOutRequest) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for AccessToken
+
+	return nil
+}
+
+// SignOutRequestValidationError is the validation error returned by
+// SignOutRequest.Validate if the designated constraints aren't met.
+type SignOutRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SignOutRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SignOutRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SignOutRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SignOutRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SignOutRequestValidationError) ErrorName() string { return "SignOutRequestValidationError" }
+
+// Error satisfies the builtin error interface
+func (e SignOutRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSignOutRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SignOutRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SignOutRequestValidationError{}
+
 // Validate checks the field values on ResetPasswordRequest with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, an error is returned.
