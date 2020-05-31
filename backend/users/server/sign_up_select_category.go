5995f1d19899de9aac41933056af2c625446e6fb
Sebastián <sebachamena@gmail.com>
2020-05-31 16:25:40 -0300

    backend/users: Add signUpSelectCategory and test

---
+++ backend/users/server/sign_up_select_category.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/athomecomar/athome/backend/users/ent/field"
+	"github.com/athomecomar/athome/backend/users/pb/pbuser"
+	"github.com/athomecomar/storeql"
+	"github.com/athomecomar/xerrors"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (s *Server) SignUpSelectCategory(ctx context.Context, in *pbuser.SignUpSelectCategoryRequest) (*emptypb.Empty, error) {
+	if err := in.Validate(); err != nil {
+		return nil, err
+	}
+	db, err := connDB()
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "connDB: %v", err)
+	}
+	defer db.Close()
+	return s.signUpSelectCategory(ctx, db, in)
+}
+
+func (s *Server) signUpSelectCategory(ctx context.Context, db *sqlx.DB, in *pbuser.SignUpSelectCategoryRequest) (e *emptypb.Empty, err error) {
+	previous, err := fetchOnboardingByToken(ctx, db, in.GetOnboardingId())
+	if errors.Is(err, sql.ErrNoRows) {
+		err = status.Errorf(xerrors.NotFound, "onboarding with id %v not found", in.GetOnboardingId())
+		return
+	}
+	if err != nil {
+		err = status.Errorf(xerrors.Internal, "fetchOnboardingByToken: %v", err)
+		return
+	}
+	onboarding := previous.Next()
+
+	code, err := onboarding.MustStage(field.SelectCategory)
+	if err != nil {
+		return nil, status.Errorf(code, "MustStage: %v", err)
+	}
+
+	code, err = onboarding.ValidateByStage(ctx, db)
+	if err != nil {
+		return nil, status.Errorf(code, "ValidateByStage: %v", err)
+	}
+
+	err = onboarding.SetCategory(ctx, db, in.GetCategoryName())
+	if err != nil {
+		err = status.Errorf(xerrors.Internal, "onboarding.SetCategory: %v", err)
+		return
+	}
+	err = storeql.UpdateIntoDB(ctx, db, onboarding)
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "UpdateIntoDB: %v", err)
+	}
+
+	return &emptypb.Empty{}, nil
+}
---
+++ backend/users/server/sign_up_select_category_test.go
@@ -0,0 +1,81 @@
+package server
+
+import (
+	"context"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/athomecomar/athome/backend/users/ent/field"
+	"github.com/athomecomar/athome/backend/users/pb/pbuser"
+	"github.com/athomecomar/semantic/semprov"
+	"github.com/athomecomar/storeql"
+	"github.com/athomecomar/storeql/test/sqlassist"
+	"github.com/athomecomar/storeql/test/sqlhelp"
+	"github.com/athomecomar/xerrors"
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc/status"
+)
+
+func TestServer_signUpSelectCategory(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		in  *pbuser.SignUpSelectCategoryRequest
+	}
+	tests := []struct {
+		name       string
+		args       args
+		queryStubs []*sqlassist.QueryStubber
+		execStubs  []*sqlassist.ExecStubber
+		wantStatus xerrors.Code
+	}{
+		{
+			name: "oor service-provider",
+			args: args{
+				ctx: context.Background(),
+				in:  &pbuser.SignUpSelectCategoryRequest{CategoryName: semprov.Medic.Name, OnboardingId: gOnboardings.ServiceProviders.Foo.Id},
+			},
+			queryStubs: []*sqlassist.QueryStubber{
+				{
+					Expect: "SELECT * FROM onboardings",
+					Rows:   sqlmock.NewRows(storeql.SQLColumns(gOnboardings.Consumers.Foo)).AddRow(storeql.SQLValues(setStage(gOnboardings.ServiceProviders.Foo, field.SelectCategory))...),
+				},
+			},
+			wantStatus: xerrors.OutOfRange,
+		},
+		{
+			name: "basic service-provider",
+			args: args{
+				ctx: context.Background(),
+				in:  &pbuser.SignUpSelectCategoryRequest{CategoryName: semprov.Medic.Name, OnboardingId: gOnboardings.ServiceProviders.Foo.Id},
+			},
+			queryStubs: []*sqlassist.QueryStubber{
+				{
+					Expect: "SELECT * FROM onboardings",
+					Rows:   sqlmock.NewRows(storeql.SQLColumns(gOnboardings.Consumers.Foo)).AddRow(storeql.SQLValues(setStage(gOnboardings.ServiceProviders.Foo, field.Shared))...),
+				},
+			},
+			execStubs: []*sqlassist.ExecStubber{
+				{Expect: "UPDATE onboardings SET", Result: sqlmock.NewResult(1, 1)},
+			},
+			wantStatus: xerrors.OK,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			db, mock := sqlhelp.MockDB(t)
+			for _, stub := range tt.queryStubs {
+				stub.Stub(mock)
+			}
+			for _, stub := range tt.execStubs {
+				stub.Stub(mock)
+			}
+			s := &Server{}
+			_, err := s.signUpSelectCategory(tt.args.ctx, db, tt.args.in)
+			if status.Code(err) != tt.wantStatus {
+				t.Fatalf("Server.signUpSelectCategory() error = %v, status: %v;  wantStatus %v", err, status.Code(err), tt.wantStatus)
+			}
+		})
+	}
+}
