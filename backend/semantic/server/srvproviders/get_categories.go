d21fb00db683ea0da2cec8c8bd4d5637a0e87373
Sebastián <sebachamena@gmail.com>
2020-06-03 17:28:03 -0300

    backend/semantic: Add getCategories rpc

---
+++ backend/semantic/server/srvproviders/get_categories.go
@@ -0,0 +1,44 @@
+package srvproviders
+
+import (
+	"context"
+
+	"github.com/athomecomar/athome/backend/semantic/ent"
+	"github.com/athomecomar/athome/backend/semantic/pb/pbsemantic"
+	"github.com/athomecomar/athome/backend/semantic/server"
+	"github.com/athomecomar/xerrors"
+	"github.com/jmoiron/sqlx"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (s *Server) GetCategories(ctx context.Context, _ *emptypb.Empty) (*pbsemantic.GetCategoriesResponse, error) {
+	db, err := server.ConnDB()
+	if err != nil {
+		return nil, err
+	}
+	return s.getCategories(ctx, db)
+}
+
+func (s *Server) getCategories(ctx context.Context, db *sqlx.DB) (*pbsemantic.GetCategoriesResponse, error) {
+	rows, err := db.QueryxContext(ctx, `SELECT * FROM service_provider_categories ORDER BY parent_id`)
+	if err != nil {
+		return nil, status.Errorf(xerrors.Internal, "QueryxContext: %v", err)
+	}
+
+	var tree ent.CategoryTree
+	for rows.Next() {
+		cat := &ent.ServiceProviderCategory{}
+		err = rows.StructScan(cat)
+		if err != nil {
+			return nil, status.Errorf(xerrors.Internal, "StructScan: %v", err)
+		}
+
+		tree, err = ent.AddCategoryToTree(tree, cat)
+		if err != nil {
+			return nil, status.Errorf(xerrors.Internal, "AddCategoryToTree: %v", err)
+		}
+	}
+
+	return server.CategoryTreeToGetCategoriesResponse(tree), nil
+}
