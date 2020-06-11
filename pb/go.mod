894bd35b4bd65dfd93d52a59608bae4c453513f1
Sebastián <sebachamena@gmail.com>
2020-06-11 18:05:35 -0300

    Add pb pkg repo

---
+++ pb/go.mod
@@ -0,0 +1,10 @@
+module github.com/athomecomar/athome/pb
+
+go 1.14
+
+require (
+	github.com/envoyproxy/protoc-gen-validate v0.3.0
+	github.com/golang/protobuf v1.4.2
+	google.golang.org/grpc v1.29.1
+	google.golang.org/protobuf v1.24.0
+)
---
+++ pb/test/pbauthtest/mock.go
@@ -0,0 +1,31 @@
+package pbauthtest
+
+import (
+	"context"
+
+	"github.com/athomecomar/pb/pbauth"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type Client struct {
+	Create   *pbauth.CreateAuthenticationResponse
+	Retrieve *pbauth.RetrieveAuthenticationResponse
+
+	CreateErr   error
+	RetrieveErr error
+	DeleteErr   error
+}
+
+func (c Client) DeleteAuthentication(ctx context.Context, in *pbauth.DeleteAuthenticationRequest, opts ...grpc.CallOption) (*empty.Empty, error) {
+	return &emptypb.Empty{}, c.DeleteErr
+}
+
+func (c Client) CreateAuthentication(ctx context.Context, in *pbauth.CreateAuthenticationRequest, opts ...grpc.CallOption) (*pbauth.CreateAuthenticationResponse, error) {
+	return c.Create, c.CreateErr
+}
+
+func (c Client) RetrieveAuthentication(ctx context.Context, in *pbauth.RetrieveAuthenticationRequest, opts ...grpc.CallOption) (*pbauth.RetrieveAuthenticationResponse, error) {
+	return c.Retrieve, c.RetrieveErr
+}
