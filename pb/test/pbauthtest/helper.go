5f1bf6cd55b808cffe3fc183bdd4c78e7755d771
Sebastián <sebachamena@gmail.com>
2020-06-12 15:23:30 -0300

    Fix pbauthtest helper

--- pb/test/pbauthtest/helper.go
+++ pb/test/pbauthtest/helper.go
@@ -2,18 +2,15 @@ package pbauthtest
 
 import (
 	"context"
-	"testing"
 
 	pbauth "github.com/athomecomar/athome/pb/pbauth"
 	gomock "github.com/golang/mock/gomock"
 )
 
-func NewCtrlFromRetrieve(t *testing.T, ctx context.Context, access string, userId uint64) *gomock.Controller {
-	t.Helper()
-	ctrl := gomock.NewController(t)
-	NewMockAuthClient(ctrl).
-		EXPECT().
+func NewCtrlFromRetrieve(ctrl *gomock.Controller, ctx context.Context, access string, userId uint64) pbauth.AuthClient {
+	c := NewMockAuthClient(ctrl)
+	c.EXPECT().
 		RetrieveAuthentication(ctx, &pbauth.RetrieveAuthenticationRequest{AccessToken: access}).
 		Return(&pbauth.RetrieveAuthenticationResponse{UserId: userId}, nil)
-	return ctrl
+	return c
 }
