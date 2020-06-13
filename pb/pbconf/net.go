ee49089f4e5cbeea89bdeb444efb04f19dca9727
Sebastián <sebachamena@gmail.com>
2020-06-13 13:37:25 -0300

    Add pbconf

---
+++ pb/pbconf/net.go
@@ -0,0 +1,53 @@
+package pbconf
+
+import "github.com/athomecomar/envconf"
+
+type service struct {
+	Name string
+}
+
+func (s *service) GetAddr() string {
+	return s.GetHost() + s.GetPort()
+}
+
+func (s *service) GetPort() (port string) {
+	switch envconf.GetENV() {
+	case envconf.Development:
+		port = devPorts[s.Name]
+	}
+	return
+}
+
+func (s *service) GetHost() (h string) {
+	switch envconf.GetENV() {
+	case envconf.Development:
+		h = s.Name + "_svc"
+	}
+	return
+}
+
+var (
+	Auth       = &service{Name: "auth"}
+	Mailer     = &service{Name: "mailer"}
+	Identifier = &service{Name: "identifier"}
+	Users      = &service{Name: "users"}
+
+	Semantic = &service{Name: "semantic"}
+	Products = &service{Name: "products"}
+	Images   = &service{Name: "images"}
+	Services = &service{Name: "services"}
+	Address  = &service{Name: "address"}
+)
+
+var devPorts = map[string]string{
+	"auth":       ":9900",
+	"mailer":     ":9901",
+	"identifier": ":9902",
+
+	"users":    ":9990",
+	"semantic": ":9991",
+	"products": ":9992",
+	"images":   ":9993",
+	"services": ":9994",
+	"address":  ":9995",
+}
