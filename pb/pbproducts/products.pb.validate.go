d52cef45722be701acc06e0842ac23cd0f1ec9c6
Sebastián <sebachamena@gmail.com>
2020-06-18 16:27:21 -0300

    pb/pbproducts: Bump to latest version

--- pb/pbproducts/products.pb.validate.go
+++ pb/pbproducts/products.pb.validate.go
@@ -36,6 +36,157 @@ var (
 // define the regex for a UUID once up-front
 var _products_uuidPattern = regexp.MustCompile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
 
+// Validate checks the field values on RetrieveProductsRequest with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, an error is returned.
+func (m *RetrieveProductsRequest) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	return nil
+}
+
+// RetrieveProductsRequestValidationError is the validation error returned by
+// RetrieveProductsRequest.Validate if the designated constraints aren't met.
+type RetrieveProductsRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e RetrieveProductsRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e RetrieveProductsRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e RetrieveProductsRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e RetrieveProductsRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e RetrieveProductsRequestValidationError) ErrorName() string {
+	return "RetrieveProductsRequestValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e RetrieveProductsRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sRetrieveProductsRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = RetrieveProductsRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = RetrieveProductsRequestValidationError{}
+
+// Validate checks the field values on RetrieveProductsResponse with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, an error is returned.
+func (m *RetrieveProductsResponse) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	for key, val := range m.GetProducts() {
+		_ = val
+
+		// no validation rules for Products[key]
+
+		if v, ok := interface{}(val).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return RetrieveProductsResponseValidationError{
+					field:  fmt.Sprintf("Products[%v]", key),
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	return nil
+}
+
+// RetrieveProductsResponseValidationError is the validation error returned by
+// RetrieveProductsResponse.Validate if the designated constraints aren't met.
+type RetrieveProductsResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e RetrieveProductsResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e RetrieveProductsResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e RetrieveProductsResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e RetrieveProductsResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e RetrieveProductsResponseValidationError) ErrorName() string {
+	return "RetrieveProductsResponseValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e RetrieveProductsResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sRetrieveProductsResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = RetrieveProductsResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = RetrieveProductsResponseValidationError{}
+
 // Validate checks the field values on SearchProductsRequest with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, an error is returned.
--- pb/test/pbproductstest/mock.go
+++ pb/test/pbproductstest/mock.go
@@ -534,6 +534,26 @@ func (mr *MockViewerClientMockRecorder) RetrieveProductDetail(arg0, arg1 interfa
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RetrieveProductDetail", reflect.TypeOf((*MockViewerClient)(nil).RetrieveProductDetail), varargs...)
 }
 
+// RetrieveProducts mocks base method
+func (m *MockViewerClient) RetrieveProducts(arg0 context.Context, arg1 *pbproducts.RetrieveProductsRequest, arg2 ...grpc.CallOption) (*pbproducts.RetrieveProductsResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "RetrieveProducts", varargs...)
+	ret0, _ := ret[0].(*pbproducts.RetrieveProductsResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// RetrieveProducts indicates an expected call of RetrieveProducts
+func (mr *MockViewerClientMockRecorder) RetrieveProducts(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RetrieveProducts", reflect.TypeOf((*MockViewerClient)(nil).RetrieveProducts), varargs...)
+}
+
 // SearchProducts mocks base method
 func (m *MockViewerClient) SearchProducts(arg0 context.Context, arg1 *pbproducts.SearchProductsRequest, arg2 ...grpc.CallOption) (*pbproducts.SearchProductsResponse, error) {
 	m.ctrl.T.Helper()
