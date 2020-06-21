ce5f251e8ac9e20c6cefd0c61ee7ab70975f5209
Sebastián <sebachamena@gmail.com>
2020-06-21 17:44:00 -0300

    pb: Add time.go status err wrapper

--- pb/pbutil/time.go
+++ pb/pbutil/time.go
@@ -1,10 +1,11 @@
 package pbutil
 
 import (
-	"errors"
 	"math"
 
 	"github.com/athomecomar/athome/pb/pbshared"
+	"github.com/athomecomar/xerrors"
+	"google.golang.org/grpc/status"
 )
 
 func DiffTimeOfDay(t1, t2 *pbshared.TimeOfDay) int64 {
@@ -16,7 +17,7 @@ func DiffTimeOfDay(t1, t2 *pbshared.TimeOfDay) int64 {
 func RestTimeOfDay(t1 *pbshared.TimeOfDay, mins int64) (*pbshared.TimeOfDay, error) {
 	hours := int64(math.Floor(float64(mins) / 60))
 	if hours > t1.GetHour() {
-		return nil, errors.New("dow time overflow")
+		return nil, status.Errorf(xerrors.InvalidArgument, "dow time overflows got %v want rest %v", t1.GetHour(), hours)
 	}
 	minute := t1.GetMinute() - (mins - hours*60)
 	if minute < 0 {
