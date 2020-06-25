b552bbecb6d69d2b20f0e02f7924f36d74e700b2
Sebastián <sebachamena@gmail.com>
2020-06-25 19:12:32 -0300

    pb/pbproducts: Bump to latest version

--- pb/pbproducts/products.pb.validate.go
+++ pb/pbproducts/products.pb.validate.go
@@ -36,6 +36,85 @@ var (
 // define the regex for a UUID once up-front
 var _products_uuidPattern = regexp.MustCompile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$")
 
+// Validate checks the field values on ConfirmReserveStockRequest with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, an error is returned.
+func (m *ConfirmReserveStockRequest) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for AccessToken
+
+	if v, ok := interface{}(m.GetOrder()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ConfirmReserveStockRequestValidationError{
+				field:  "Order",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
+	return nil
+}
+
+// ConfirmReserveStockRequestValidationError is the validation error returned
+// by ConfirmReserveStockRequest.Validate if the designated constraints aren't met.
+type ConfirmReserveStockRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ConfirmReserveStockRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ConfirmReserveStockRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ConfirmReserveStockRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ConfirmReserveStockRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ConfirmReserveStockRequestValidationError) ErrorName() string {
+	return "ConfirmReserveStockRequestValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ConfirmReserveStockRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sConfirmReserveStockRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ConfirmReserveStockRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ConfirmReserveStockRequestValidationError{}
+
 // Validate checks the field values on ReserveStockRequest with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, an error is returned.
