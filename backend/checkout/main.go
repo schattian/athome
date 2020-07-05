828b702b603dcc23053383cd0c6d67bea8b96a14
Sebastián <sebachamena@gmail.com>
2020-07-05 19:14:43 -0300

    backend/checkout: Register srvpayments and srvshippings

--- backend/checkout/main.go
+++ backend/checkout/main.go
@@ -5,7 +5,9 @@ import (
 	"net"
 
 	"github.com/athomecomar/athome/backend/checkout/server/srvcards"
+	"github.com/athomecomar/athome/backend/checkout/server/srvpayments"
 	"github.com/athomecomar/athome/backend/checkout/server/srvpurchases"
+	"github.com/athomecomar/athome/backend/checkout/server/srvshippings"
 	"github.com/athomecomar/athome/pb/pbcheckout"
 	"github.com/athomecomar/athome/pb/pbconf"
 	"google.golang.org/grpc"
@@ -21,6 +23,8 @@ func main() {
 	s := grpc.NewServer()
 	log.Println("listening on port " + port)
 	pbcheckout.RegisterCardsServer(s, &srvcards.Server{})
+	pbcheckout.RegisterPaymentsServer(s, &srvpayments.Server{})
+	pbcheckout.RegisterShippingsServer(s, &srvshippings.Server{})
 	pbcheckout.RegisterPurchasesServer(s, &srvpurchases.Server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
